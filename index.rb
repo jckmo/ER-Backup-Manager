80e962582896dababc115690aeb67ab806a86848
jckmo <jacksonmo@me.com>
2023-07-02 18:42:50 -0400

    auto load if only one build exists per in a level folder

--- index.rb
+++ index.rb
@@ -32,8 +32,6 @@ class ERBackup
   def self.load_file
     options = Dir.entries("#{@@backup_folder}")
 
-
-    puts $line
     puts $line
     puts "\t SELECT BACKUP"
     puts $line
@@ -44,11 +42,29 @@ class ERBackup
 
     input1 = gets.chomp
     case input1
-      when input1
-        if Array(options).include? input1
-          options2 = Dir.entries("#{@@backup_folder}/#{input1}")
+    when input1
 
-          puts $line
+      if Array(options).include? input1
+        options2 = Dir.entries("#{@@backup_folder}/#{input1}")
+        puts options2.length()
+        if Array(options2).length == 3
+          input2 = options2[2]
+          full_path = "#{@@backup_folder}/#{input1}/#{input2}"
+
+          puts "You are about to save the current game file and replace it with the file at #{full_path}. \n\t Continue? (y/n)"
+          input3 = gets.chomp
+
+          case input3
+          when 'y'
+            ERBackup.backup_current_file()
+            FileUtils.copy_file("#{@@backup_folder}/#{input1}/#{input2}/#{@@game_file_name}", "#{@@game_folder}/#{@@game_file_name}")
+            File.open("#{@@backup_folder}/misc/Current.txt", 'w') { |file| file.write("#{input1} #{input2}") }
+            puts "#{full_path} loaded"
+          else
+            ERBackup.load_file()
+          end
+
+        else 
           puts $line
           puts "\t SELECT BACKUP"
           puts $line
@@ -65,37 +81,42 @@ class ERBackup
             puts $line
             puts "You are about to save the current game file and replace it with the file at #{full_path}. \n\t Continue? (y/n)"
             input3 = gets.chomp
+
             case input3
             when 'y'
               puts "\n"
               ERBackup.backup_current_file()
               FileUtils.copy_file("#{@@backup_folder}/#{input1}/#{input2}/#{@@game_file_name}", "#{@@game_folder}/#{@@game_file_name}")
-              File.open("#{@@backup_folder}/Current.txt", 'w') { |file| file.write("#{input1} #{input2}") }
+              File.open("#{@@backup_folder}/misc/Current.txt", 'w') { |file| file.write("#{input1} #{input2}") }
               puts "#{full_path} loaded"
               puts "\n"
             else
               ERBackup.load_file()
             end
+
           elsif input2 == 'exit'
             return nil
+
           else
             puts 'try again'
             sleep(1.2)
             ERBackup.load_file()
           end
-        elsif input1 == 'exit'
-          return nil
-        else
-            puts 'try again'
-            sleep(1.2)
-            ERBackup.load_file()
         end
+
+      elsif input1 == 'exit'
+        return nil
       else
+          puts 'try again'
+          sleep(1.2)
+          ERBackup.load_file()
+      end
+    else
     end
   end
 end
 
-current_game = file_data = File.read("./EldenRingBackup/Current.txt").split.join(" ")
+current_game = file_data = File.read("./EldenRingBackup/misc/Current.txt").split.join(" ")
 
 puts $line
 puts "\nHello! Welcome to ER Backup Manager. Please refer to documentation for desired file structure"
